beeee7c752f809ec4ec6c81b61ffa2b04054768e
JCBackendDev <jcbackenddev@gmail.com>
2021-12-08 17:02:53 +0100

    added dynamic input of data
    We shall always use this wonderful little ngFor loop, such a bless

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'auCards';
+  posts = [
+    {
+      imageUrl: '../assets/tree.jpeg',
+      title: 'Some radom tree',
+      username: 'treelover',
+      content: 'This is a treelovers lovers tree'
+    },
+    {
+      imageUrl: '../assets/mountain.jpeg',
+      title: 'Some radom mountain',
+      username: 'mountainlover',
+      content: 'This is a mountain for lovers'
+    },
+    {
+      imageUrl: '../assets/biking.jpeg',
+      title: 'Some radom biking',
+      username: 'biker11',
+      content: 'This is what I am talking about!'
+    }
+  ];
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  @Input() imageUrl= '';
+  @Input() title= '';
+  @Input() username= '';
+  @Input() content= '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
